2d6f4bba3f9a8487e55d83e18bd5bb7e34fdc274
Fatima Mohamed <fatima.mohamed@adu.ac.ae>
2026-02-10 11:45:43 +0400

    Fixed server.js

--- src/server.js
+++ src/server.js
@@ -14,7 +14,9 @@ const app = express();
 //app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(express.static(path.join(process.cwd(), "public")));
 
-
+app.get("/", (req, res) => {
+  res.sendFile(path.join(process.cwd(), "public", "index.html"));
+});
 
 
 app.use(express.json({ limit: "1mb" }));
